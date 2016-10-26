4cc22d806549345da08a564532b9ee3d591c5354
tylertaylor <vikingtyty@gmail.com>
2016-10-26 09:09:05 -0500

    can successfully add new movie through director, started show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,41 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show]
 
   def new
+    # binding.pry
     @movie = Movie.new
+    @director = @movie.build_director
+    # binding.pry
+    if params[:director_id]
+      @director = Director.find(params[:director_id])
+    end
+  end
+
+  def create
+    # binding.pry
+    @movie = Movie.new(movie_params)
+    @director = Director.find_by(name: params[:movie][:director_attributes][:name])
+    # binding.pry
+    if @movie.save
+      redirect_to movie_path(@movie)
+    else
+      # redirect_to(:back, :flash => { :error => @movie.errors.full_messages })
+      # redirect_to new_director_movie_path(@director), :flash => { :error => @movie.errors.full_messages }
+      render "new", location: new_director_movie_path(@director)
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title, :director_attributes => [:name])
   end
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,13 @@ class Movie < ActiveRecord::Base
   has_many :actors, :through => :roles
   belongs_to :director
   has_many :ratings
+
+  validates :title, presence: true
+
+  def director_attributes=(director_attribute)
+    # binding.pry
+    # self.build_director(director_attribute)
+    self.director = Director.where(:name => director_attribute[:name]).first_or_create
+    binding.pry
+  end
 end
