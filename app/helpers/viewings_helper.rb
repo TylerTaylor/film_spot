a50b9659bff650217fd731d8d28e70941f33cb15
tylertaylor <vikingtyty@gmail.com>
2016-10-29 19:27:40 -0500

    added view status helper method to say whether a movie is seen or not, some changes to styling

--- app/helpers/viewings_helper.rb
+++ app/helpers/viewings_helper.rb
@@ -4,4 +4,12 @@ module ViewingsHelper
       Viewing.exists?(:user_id => current_user.id, :movie_id => movie.id)
     end
   end
+
+  def view_status(movie)
+    if viewed?(movie)
+      "(Seen by you)"
+    else
+      "(Unseen by you)"
+    end
+  end
 end
