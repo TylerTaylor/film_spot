ffc336b8ec3a88ecf13abe6f5f1c4fa269143dc8
tylertaylor <vikingtyty@gmail.com>
2016-12-28 11:39:43 -0600

    working on loading rating form via AJAX

---
+++ app/assets/javascripts/viewings.js
@@ -0,0 +1,37 @@
+console.log("yeehaw we viewing.js")
+
+$(function(){
+  $("a.clicked-yes").on("click", function(e) {
+    // fire some ajax
+    // debugger;
+    $.ajax({
+      method: "GET",
+      url: this.href // what URL should we fire this request to?
+    }).done(function(data){
+      // get a response (data)
+      // debugger;
+      $(".viewing").html("").append(data);
+      // load that response into the HTML of the page
+    })
+
+
+    e.preventDefault();
+  });
+
+  
+});
+
+
+// $(function(){
+//   bindClick()
+// })
+
+// function bindClick() {
+//   debugger;
+//   $("a.clicked-yes").on("click", function(e) {
+//     e.preventDefault();
+//     $.get(this.href).success(function(response){
+//       $(".viewing").html("").append(response);
+//     })
+//   })
+// }
