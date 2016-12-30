de36fdbcb551cb2a1a007e5b12aac6f75321852a
tylertaylor <vikingtyty@gmail.com>
2016-12-30 14:43:00 -0600

    refactored viewings.js from .ajax call to .get call

--- app/assets/javascripts/viewings.js
+++ app/assets/javascripts/viewings.js
@@ -1,37 +1,36 @@
 console.log("yeehaw we viewing.js")
 
-$(function(){
-  $("a.clicked-yes").on("click", function(e) {
-    // fire some ajax
-    // debugger;
-    $.ajax({
-      method: "GET",
-      url: this.href // what URL should we fire this request to?
-    }).done(function(data){
-      // get a response (data)
-      // debugger;
-      $(".viewing").html("").append(data);
-      // load that response into the HTML of the page
-    })
+// $(function(){
+//   $("a.clicked-yes").on("click", function(e) {
+//     // fire some ajax
+//     // debugger;
+//     $.ajax({
+//       method: "GET",
+//       url: this.href // what URL should we fire this request to?
+//     }).done(function(data){
+//       // get a response (data)
+//       // debugger;
+//       $(".viewing").html("").append(data);
+//       // load that response into the HTML of the page
+//     })
 
 
-    e.preventDefault();
-  });
+//     e.preventDefault();
+//   });
 
   
-});
+// });
 
 
-// $(function(){
-//   bindClick()
-// })
+$(function(){
+  bindClick()
+})
 
-// function bindClick() {
-//   debugger;
-//   $("a.clicked-yes").on("click", function(e) {
-//     e.preventDefault();
-//     $.get(this.href).success(function(response){
-//       $(".viewing").html("").append(response);
-//     })
-//   })
-// }
+function bindClick() {
+  $("a.clicked-yes").on("click", function(e) {
+    e.preventDefault();
+    $.get(this.href).success(function(response){
+      $(".viewing").html("").append(response);
+    })
+  })
+}
