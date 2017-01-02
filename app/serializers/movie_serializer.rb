f4d2635f1ae0e6dcb5ea29fad672a75099d81123
tylertaylor <vikingtyty@gmail.com>
2017-01-02 10:30:16 -0600

    added description to movie serializer

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,5 +1,5 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id, :title, :description
   # belongs_to :director, serializer: MovieDirectorSerializer
   has_one :director
 end
