4e4e53cd8a2b9f31bd5ca82fa894770c508cb587
tylertaylor <vikingtyty@gmail.com>
2017-01-02 16:02:32 -0600

    movie serializer now knows about a movies actors

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -2,4 +2,5 @@ class MovieSerializer < ActiveModel::Serializer
   attributes :id, :title, :description
   # belongs_to :director, serializer: MovieDirectorSerializer
   has_one :director
+  has_many :actors
 end
