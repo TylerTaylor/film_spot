f6795a622e0da5b52a15ed0b67cae42dfacdc5b8
tylertaylor <vikingtyty@gmail.com>
2017-01-04 11:51:42 -0600

    director-js ajax call now successfully creates a director object based on the link clicked from the directors index view

--- app/assets/javascripts/directors.js
+++ app/assets/javascripts/directors.js
@@ -2,12 +2,18 @@ $(function(){
   // grab the director link
   $(".director-js").on("click", function(e){
     e.preventDefault();
-    $.getJSON(this.href).success(function(response){
-      debugger;
+    $.getJSON(this.href).success(function(resp){
+      let director = new Director(resp.id, resp.movies, resp.name)
+      
     })
   })
 })
 
 // make director object
+function Director(id, movies, name) {
+  this.id = id
+  this.movies = movies
+  this.name = name
+}
 
 // format director's movies
