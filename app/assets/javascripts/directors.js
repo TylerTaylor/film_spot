3420f53e5c50d1086ce6d994fde26be8b9b50180
tylertaylor <vikingtyty@gmail.com>
2017-01-04 15:38:22 -0600

    directors.js now has a toggleMovies prototype function for Director objects, displays and hides the director's movies from the director show page via AJAX and JSON

--- app/assets/javascripts/directors.js
+++ app/assets/javascripts/directors.js
@@ -5,7 +5,10 @@ $(function(){
     $.getJSON(this.href).success(function(resp){
       let director = new Director(resp.id, resp.movies, resp.name)
       let movies = director.formatMovies()
-      
+
+      director.toggleMovies()
+      // toggleMovies(director, movies)
+      // $(`.director-${director.id}`).append(movies)
     })
   })
 })
@@ -19,12 +22,36 @@ function Director(id, movies, name) {
 
 // format director's movies
 Director.prototype.formatMovies = function() {
-  html = ''
+  html = '<div class="director-movies">'
   let movies = this.movies
 
   for (let i = 0; i < movies.length; i++) {
-    html += `<a href="/movies/${movies[i].id}">${movies[i].title}</a>`
+    html += `<a href="/movies/${movies[i].id}">${movies[i].title}</a><br>`
   }
 
+  html += '<br></div>'
+
   return html
+}
+
+let showingMovies = false
+
+// function toggleMovies(director, formattedMovies) {
+//   if (showingMovies == false) { // if we aren't currently displaying a director's movies
+//     $(`.director-${director.id}`).append(formattedMovies); // display them
+//     showingMovies = true
+//   } else if (showingMovies == true) { // if the movies are displaying
+//     $('.director-movies').remove() // remove the div
+//     showingMovies = false 
+//   }
+// }
+
+Director.prototype.toggleMovies = function() {
+  if (showingMovies == false) { // if we aren't currently displaying a director's movies
+    $(`.director-${this.id}`).append(this.formatMovies()); // display them
+    showingMovies = true
+  } else if (showingMovies == true) { // if the movies are displaying
+    $('.director-movies').remove() // remove the div
+    showingMovies = false 
+  }
 }
