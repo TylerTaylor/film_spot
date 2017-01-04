ef6882d304c3be680aca8a807b35171917e00e3b
tylertaylor <vikingtyty@gmail.com>
2017-01-04 12:31:33 -0600

    created formatMovies prototype function in directors.js

--- app/assets/javascripts/directors.js
+++ app/assets/javascripts/directors.js
@@ -4,6 +4,7 @@ $(function(){
     e.preventDefault();
     $.getJSON(this.href).success(function(resp){
       let director = new Director(resp.id, resp.movies, resp.name)
+      let movies = director.formatMovies()
       
     })
   })
@@ -17,3 +18,13 @@ function Director(id, movies, name) {
 }
 
 // format director's movies
+Director.prototype.formatMovies = function() {
+  html = ''
+  let movies = this.movies
+
+  for (let i = 0; i < movies.length; i++) {
+    html += `<a href="/movies/${movies[i].id}">${movies[i].title}</a>`
+  }
+
+  return html
+}
